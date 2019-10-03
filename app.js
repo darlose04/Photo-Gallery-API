baf38b2e3b381ee05311f25e24260c23aa948972
Zach Smith <zsmith126028@gmail.com>
2019-10-03 17:42:50 -0500

    import photos array, comment out cors

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
+// const imagesRouter = require("./controllers/images");
+const photos = require("./photos");
 
-app.use(cors);
+// app.use(cors);
 app.use(bodyParser.json());
+
+// app.use("/api/images", imagesRouter);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Landing Page</h1>");
+});
+
+app.get("/images", (req, res) => {
+  res.json(photos);
+});
 
 module.exports = app;
