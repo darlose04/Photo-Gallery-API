8a1de612065678080c35c96b36a267c344f79763
Zach Smith <zsmith126028@gmail.com>
2019-10-03 17:01:29 -0500

    add photos that will eventually be placed in db

---
+++ photos.js
@@ -0,0 +1,179 @@
+const photos = [
+  {
+    id: 1,
+    name: "Cotton Candy Sunrise",
+    src: cottoncandy,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 2,
+    name: "Deserted Railroad",
+    src: railroad,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 3,
+    name: "Snake River Overlook",
+    src: snakeriver,
+    orientation: "portrait",
+    price: "$25.00",
+    width: 3,
+    height: 4
+  },
+  {
+    id: 4,
+    name: "Grand Prismatic Spring",
+    src: grandspring,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 5,
+    name: "Lone Eagle Sunset",
+    src: loneeagle,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 6,
+    name: "On the Shoulders of Giants",
+    src: giantshoulders,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 7,
+    name: "Reflections",
+    src: reflections,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 8,
+    name: "Chasing Waterfalls",
+    src: waterfalls,
+    orientation: "portrait",
+    price: "$25.00",
+    width: 3,
+    height: 4
+  },
+
+  {
+    id: 9,
+    name: "Last Light",
+    src: lastlight,
+    orientation: "portrait",
+    price: "$25.00",
+    width: 3,
+    height: 4
+  },
+
+  {
+    id: 10,
+    name: "Rustic Barn",
+    src: rustic,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 11,
+    name: "The Archway to the West",
+    src: archway,
+    orientation: "portrait",
+    price: "$25.00",
+    width: 3,
+    height: 4
+  },
+  {
+    id: 12,
+    name: "Schwabacher Sunrise",
+    src: schwabacher,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 13,
+    name: "Eye to Eye",
+    src: eyetoeye,
+    orientation: "portrait",
+    price: "$25.00",
+    width: 3,
+    height: 4
+  },
+  {
+    id: 14,
+    name: "Switzerland of America",
+    src: swissamerica,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 15,
+    name: "The Upside Down",
+    src: upsidedown,
+    orientation: "portrait",
+    price: "$25.00",
+    width: 3,
+    height: 4
+  },
+  {
+    id: 16,
+    name: "Teton Sunrise",
+    src: teton,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 17,
+    name: "The Hazy Bells",
+    src: hazybells,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 18,
+    name: "The Majestics Flatirons",
+    src: flatirons,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  },
+  {
+    id: 19,
+    name: "The Window to the West",
+    src: windowwest,
+    orientation: "landscape",
+    price: "$25.00",
+    width: 4,
+    height: 3
+  }
+];
+
+module.exports = {
+  photos
+};
