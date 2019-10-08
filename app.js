af75ff908c286de5f65dfcfb989fd1cab903846c
Zach Smith <zsmith126028@gmail.com>
2019-10-08 12:03:37 -0500

    comment out code that is not needed right now

--- app.js
+++ app.js
@@ -5,19 +5,13 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 // const imagesRouter = require("./controllers/images");
-const photos = require("./photos");
+// const photos = require("./photos");
 
 // app.use(cors);
 app.use(bodyParser.json());
 
-// app.use("/api/images", imagesRouter);
-
 app.get("/", (req, res) => {
   res.send("<h1>Landing Page</h1>");
-});
-
-app.get("/images", (req, res) => {
-  res.json(photos);
 });
 
 module.exports = app;
