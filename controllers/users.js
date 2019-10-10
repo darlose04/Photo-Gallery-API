cbc2b87b075d5b59a5298fce124905b957759cf1
Zach Smith <zsmith126028@gmail.com>
2019-10-10 13:25:24 -0500

    add register route

--- controllers/users.js
+++ controllers/users.js
@@ -6,4 +6,10 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
+// register
+// login page
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
 module.exports = router;
