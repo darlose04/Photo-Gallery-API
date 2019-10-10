6c8c0de531fcfa755c4973628132885b9a1a2367
Zach Smith <zsmith126028@gmail.com>
2019-10-10 16:46:24 -0500

    add middleware for express-sessions and connect-flash

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const expressLayouts = require("express-ejs-layouts");
+const flash = require("connect-flash");
+const session = require("express-sessions");
 
 mongoose
   .connect(config.MONGODB_URI, {
@@ -22,7 +24,20 @@ mongoose
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 
+// body parser
 app.use(express.urlencoded({ extended: false }));
+
+// express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// connect flash
+app.use(flash());
 
 // app.use(cors);
 // app.use(bodyParser.json());
