444c867c123242e8393467e4b8ead5c02c62672d
Zach Smith <zsmith126028@gmail.com>
2019-10-10 15:52:36 -0500

    add validation to check register form data

--- controllers/users.js
+++ controllers/users.js
@@ -11,4 +11,37 @@ router.get("/register", (req, res) => {
   res.render("register");
 });
 
+// register handle
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  // check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
+
+  // check that passwords match
+  if (password !== password2) {
+    errors.push({ msg: "Passwords do not match" });
+  }
+
+  // check password length
+  if (password.length < 6) {
+    errors.push({ msg: "Password needs to be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    res.send("pass");
+  }
+});
+
 module.exports = router;
