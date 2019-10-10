14c964ea9a40104e12007ac52a3503cb754b7fe8
Zach Smith <zsmith126028@gmail.com>
2019-10-10 13:26:27 -0500

    uncomment index route

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// router.get("/", (req, res) => {
-//   res.send("<h1>Landing Page</h1>");
-// });
+router.get("/", (req, res) => {
+  res.send("welcome");
+});
 
 module.exports = router;
