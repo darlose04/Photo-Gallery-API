12a15ea2595bef3bf7ab5313f10be81892c24bdd
Zach Smith <zsmith126028@gmail.com>
2019-10-10 13:24:24 -0500

    uncomment index

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
 
 // Routes
-// app.use("/", require("./controllers/index"));
+app.use("/", require("./controllers/index"));
 app.use("/users", require("./controllers/users"));
 
 module.exports = app;
