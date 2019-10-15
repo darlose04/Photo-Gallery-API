91bdde00d5bdf770817bbc2faceef83639ad6e71
Zach Smith <zsmith126028@gmail.com>
2019-10-15 11:10:48 -0500

    add user name to dashboard render

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,9 @@ router.get("/", (req, res) => {
 });
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  res.render("dashboard");
+  res.render("dashboard", {
+    name: req.user.name
+  });
 });
 
 module.exports = router;
