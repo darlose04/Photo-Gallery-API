c13fc6f57386d50e582da50d41514b564524278b
Zach Smith <zsmith126028@gmail.com>
2019-10-15 11:03:43 -0500

    add logout handle

--- controllers/users.js
+++ controllers/users.js
@@ -91,4 +91,11 @@ router.post("/login", (req, res, next) => {
   })(req, res, next);
 });
 
+// logout handle
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/login");
+});
+
 module.exports = router;
