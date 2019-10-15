2433bf387954503f0a9e49d4ba6b439a7f78a7d0
Zach Smith <zsmith126028@gmail.com>
2019-10-15 12:35:03 -0500

    remove console.log

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,6 @@ router.get("/register", (req, res) => {
 router.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
   let errors = [];
-  console.log(User.length);
 
   if (User.length > 0) {
     errors.push({ msg: "You are not authorized to register" });
