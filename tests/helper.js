bcb16c83d375eb7d67c5a43ef27fc281a35a119d
Zach Smith <zsmith126028@gmail.com>
2019-10-16 11:58:28 -0500

    create function to find users in db

--- tests/helper.js
+++ tests/helper.js
@@ -1,13 +1,19 @@
 const User = require("../models/User");
 
-const initialUsers = [
-  {
-    name: "John Doe",
-    email: "jdoe@gmail.com",
-    password: "123456"
-  }
-];
+// const initialUsers = [
+//   {
+//     name: "John Doe",
+//     email: "jdoe@gmail.com",
+//     password: "123456"
+//   }
+// ];
+
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map(user => user.toJSON());
+};
 
 module.exports = {
-  initialUsers
+  // initialUsers,
+  usersInDb
 };
