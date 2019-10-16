6f083ed8b21012ad87ddb4ec6274741279a1fac6
Zach Smith <zsmith126028@gmail.com>
2019-10-16 16:57:13 -0500

    require packages needed for image uploading

--- controllers/images.js
+++ controllers/images.js
@@ -1,5 +1,10 @@
-const imagesRouter = require("express").Router();
+const router = require("express").Router();
+const path = require("path");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
 
-module.exports = {
-  imagesRouter
-};
+module.exports = router;
