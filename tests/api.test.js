94128b056e30134e9ec8c3da0ad5e99106e855c0
Zach Smith <zsmith126028@gmail.com>
2019-10-16 15:16:13 -0500

    test adding user to db. Haven't figured out how to test the actual post route. Just keeps returning 500 Internal server error. No idea how to test that at this point, no idea what is going wrong

--- tests/api.test.js
+++ tests/api.test.js
@@ -5,44 +5,6 @@ const api = supertest(app);
 const helper = require("./helper");
 const User = require("../models/User");
 
-describe("testing users in the db", () => {
-  beforeEach(async () => {
-    await User.deleteMany({});
-    const user = new User({
-      name: "John Doe",
-      email: "jdoe@gmail.com",
-      password: "123456"
-    });
-    await user.save();
-  });
-
-  test("number of users in the db", async () => {
-    const numOfUsers = await helper.usersInDb();
-
-    expect(numOfUsers.length).toBe(1);
-    expect();
-  });
-
-  test("creating a new user", async () => {
-    await User.deleteMany({});
-    const usersAtStart = await helper.usersInDb();
-
-    const newUser = new User({
-      name: "Joe",
-      email: "joe@gmail.com",
-      password: "abcdef"
-    });
-
-    await newUser.save();
-
-    const usersAtEnd = await helper.usersInDb();
-    expect(usersAtEnd.length).toBe(usersAtStart.length + 1);
-
-    const emails = usersAtEnd.map(user => user.email);
-    expect(emails).toContain(newUser.email);
-  });
-});
-
 describe("check that routes are returning as expected", () => {
   test("landing page at root route is returned successfully", async () => {
     await api.get("/").expect(200);
@@ -62,6 +24,44 @@ describe("check that routes are returning as expected", () => {
 
   test("logout page is found or returned successfully", async () => {
     await api.get("/users/logout").expect(302);
+  });
+});
+
+describe("testing users in the db", () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+    const user = new User({
+      name: "John Doe",
+      email: "jdoe@gmail.com",
+      password: "123456"
+    });
+    await user.save();
+  });
+
+  test("number of users in the db", async () => {
+    const numOfUsers = await helper.usersInDb();
+
+    expect(numOfUsers.length).toBe(1);
+  });
+
+  test("creating a new user", async () => {
+    await User.deleteMany({});
+    const usersAtStart = await helper.usersInDb();
+
+    const newUser = new User({
+      name: "Joe",
+      email: "joe@gmail.com",
+      password: "123456"
+    });
+
+    await newUser.save();
+
+    const usersAtEnd = await helper.usersInDb();
+    console.log(usersAtEnd);
+    expect(usersAtEnd.length).toBe(usersAtStart.length + 1);
+
+    const emails = usersAtEnd.map(user => user.email);
+    expect(emails).toContain(newUser.email);
   });
 });
 
