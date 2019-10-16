628187e99a7101be624676f2758ea23e5a356f62
Zach Smith <zsmith126028@gmail.com>
2019-10-16 10:45:27 -0500

    add helper file for testing database

---
+++ tests/helper.js
@@ -0,0 +1,13 @@
+const User = require("../models/User");
+
+const initialUsers = [
+  {
+    name: "John Doe",
+    email: "jdoe@gmail.com",
+    password: "123456"
+  }
+];
+
+module.exports = {
+  initialUsers
+};
