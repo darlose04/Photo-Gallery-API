246769872c4e9cc54775b156f0af9388eeca5d3e
Zach Smith <zsmith126028@gmail.com>
2019-10-16 17:33:47 -0500

    add storage engine for multer upload

--- controllers/images.js
+++ controllers/images.js
@@ -6,5 +6,41 @@ const multer = require("multer");
 const GridFsStorage = require("multer-gridfs-storage");
 const Grid = require("gridfs-stream");
 const methodOverride = require("method-override");
+const config = require("../utils/config");
+const { ensureAuthenticated } = require("../utils/auth");
+
+// mongo connection for image uploading
+const conn = mongoose.createConnection(config.MONGODB_URI);
+
+// init variable for gridfs stream
+let gfs;
+
+conn.once("open", () => {
+  // initialize the stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+// create storage engine
+const storage = new GridFsStorage({
+  url: config.MONGODB_URI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads"
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
 
 module.exports = router;
