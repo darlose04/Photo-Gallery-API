831ed311525caaece6d0b8bbd6ed3023c9cf7fb7
Zach Smith <zsmith126028@gmail.com>
2019-10-16 10:50:14 -0500

    add tests to find dashboard and logout pages

--- tests/api.test.js
+++ tests/api.test.js
@@ -2,12 +2,21 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const api = supertest(app);
-// use helper here if needed
+const helper = require("./helper");
+const User = require("../models/User");
 
-// require schema here
+// beforeEach(async () => {
+//   await User.deleteMany({});
+
+//   const userObjects = helper.
+// })
 
 test("landing page at root route is returned successfully", async () => {
   await api.get("/").expect(200);
+});
+
+test("dashboard page returned successfully", async () => {
+  await api.get("/dashboard").expect(302);
 });
 
 test("login page returned successfully", async () => {
@@ -16,6 +25,10 @@ test("login page returned successfully", async () => {
 
 test("register page returned successfully", async () => {
   await api.get("/users/register").expect(200);
+});
+
+test("logout page is found or returned successfully", async () => {
+  await api.get("/users/logout").expect(302);
 });
 
 // mongoose.connection.close();
