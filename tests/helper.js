f6d1049f1cd491a91a815c5a7db2422d2000e0c6
Zach Smith <zsmith126028@gmail.com>
2019-10-16 15:16:39 -0500

    function to return number of users in db

--- tests/helper.js
+++ tests/helper.js
@@ -10,7 +10,7 @@ const User = require("../models/User");
 
 const usersInDb = async () => {
   const users = await User.find({});
-  return users.map(user => user.toJSON());
+  return users;
 };
 
 module.exports = {
