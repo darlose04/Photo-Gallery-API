2680ad3a9116b0bd61a78c2c2c2eaf5d75105bd2
Zach Smith <zsmith126028@gmail.com>
2019-10-17 09:40:12 -0500

    add storage engine

--- controllers/images.js
+++ controllers/images.js
@@ -22,4 +22,25 @@ conn.once("open", () => {
   gfs.collection("uploads");
 });
 
+// create storage engine
+const storage = new GridFsStorage({
+  url: config.IMAGE_URI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads"
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
 module.exports = router;
