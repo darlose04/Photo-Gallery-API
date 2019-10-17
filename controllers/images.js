d6c99e9d58d95ceee5bae37e6f8378828392b48e
Zach Smith <zsmith126028@gmail.com>
2019-10-17 10:02:49 -0500

    add route for loading the form

--- controllers/images.js
+++ controllers/images.js
@@ -43,4 +43,31 @@ const storage = new GridFsStorage({
 });
 const upload = multer({ storage });
 
+// loads form
+router.get("/", ensureAuthenticated, (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    if (!files || files.length === 0) {
+      res.render("images", { files: false });
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === "image/jpeg" ||
+          file.contentType === "image/png"
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+
+      res.render("images", { files: files });
+    }
+  });
+});
+
+// upload
+router.post("/upload", upload.single("file"), (req, res) => {
+  res.json({ file: req.file });
+});
+
 module.exports = router;
