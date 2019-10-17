87a78ddf43b123cf487453305c2afaffe9d10791
Zach Smith <zsmith126028@gmail.com>
2019-10-17 14:36:51 -0500

    create new env vars

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 let PORT = process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
 let IMAGE_URI = process.env.IMAGE_URI;
+let EMAIL = process.env.EMAIL;
+let PASSWORD = process.env.PASSWORD;
 
 if (process.env.NODE_ENV === "test") {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
@@ -11,5 +13,7 @@ if (process.env.NODE_ENV === "test") {
 module.exports = {
   IMAGE_URI,
   MONGODB_URI,
-  PORT
+  PORT,
+  EMAIL,
+  PASSWORD
 };
