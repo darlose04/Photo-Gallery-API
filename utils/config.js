62219127c3ebd9f04b55343b6d57b6650dd64337
Zach Smith <zsmith126028@gmail.com>
2019-10-17 08:56:19 -0500

    add new URI for images

--- utils/config.js
+++ utils/config.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 let PORT = process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
+let IMAGE_URI = process.env.IMAGE_URI;
 
 if (process.env.NODE_ENV === "test") {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
 
 module.exports = {
+  IMAGE_URI,
   MONGODB_URI,
   PORT
 };
